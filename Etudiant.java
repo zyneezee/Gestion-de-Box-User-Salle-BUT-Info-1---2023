7b073aa4cb105edc160b0828f1e550010c90aefe
Khadjou Youssef <o22301854@info27-11.iut45.univ-orleans.fr>
2024-03-15 16:26:07 +0100

    Fichier Etudiant.java

---
+++ Etudiant.java
@@ -0,0 +1,33 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Etudiant {
+    private int numEtudiant;
+    private String prenom;
+    private String nom;
+    private List<Groupe> lesGroupes;
+    private List<Note> lesNotes;
+
+    public Etudiant(int numEtudiant , String prenom , String nom ){
+        this.numEtudiant=numEtudiant;
+        this.prenom=prenom;
+        this.nom=nom;
+        this.lesGroupes = new ArrayList<>();
+        this.lesNotes = new ArrayList<>();
+    }
+    public int getNumEtudiant(){
+        return this.numEtudiant;
+    }
+    public String getPrenom(){
+        return this.prenom;
+    }
+    public String getNom(){
+        return this.nom;
+    }
+    public void ajouteGroupe(Groupe groupe){
+        this.lesGroupes.add(groupe);
+    }
+    public void ajouteNote(Note note){
+        this.lesNotes.add(note);
+    }
+}
